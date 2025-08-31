6c8372b8280924fdd193c9dad1b55e22a4281a81
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2025-08-31 18:50:42 -0300

    31/08

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-function criaCartao (categoria, pergunta, resposta) {
-    
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('#container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+
+    cartao.innerHTML`
+<div class="cartao__conteudo">
+<h3>CATEGORIA</h3>
+<div class="cartao__conteudo__pergunta">
+<p>PERGUNTA (escrever aqui a pergunta)</p>
+</div>
+<div class="cartao__conteudo__resposta">
+<p>RESPOSTA (escrever aqui a resposta da pergunta)</p>
+</div>
+`
+
+container.appendChild(cartao)
+
 }
+
